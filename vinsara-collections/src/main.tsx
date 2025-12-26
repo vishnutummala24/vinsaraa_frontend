643503ea904b66c4e754e97ad44a7a16ea510830
vishnutummala24 <vishnutummala24@gmail.com>
2025-12-26 11:41:35 +0530

    Update main.tsx

--- vinsara-collections/src/main.tsx
+++ vinsara-collections/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google'; // <--- IMPORT
 
 // Replace with your ACTUAL Client ID from Step 0
-const GOOGLE_CLIENT_ID = "44114483480-qullmaqkm953nief34vhmmtik3g4d2e3.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "958832349798-rhb1np3nmtvm11a7edcrulor7aci1cs7.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
